6fdffbb1304db888260bf286a3235074d59b2618
Fadhl Nader <>
2024-06-13 11:57:09 +0200

    Xcode project Init

---
+++ LBCTechnicalTest/LBCTechnicalTest/LBCTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBCTechnicalTestApp.swift
+//  LBCTechnicalTest
+//
+//  Created by Fadhl Nader on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LBCTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
